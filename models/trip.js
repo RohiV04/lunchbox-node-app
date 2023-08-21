b55efc5aa713fcc4a2f1269dff4c409679b808e7
RohiV04 <rohith_csd@srkrec.edu.in>
2023-08-21 14:43:18 +0530

    Fixed The Bug In Trip Model

--- models/trip.js
+++ models/trip.js
@@ -1,16 +1,17 @@
 const db = require('../con');
 
-
 const tripModel = {
-  getTripByPhoneNumber: (phoneNumber) => {
+  getTripsByPhoneNumber: (phoneNumber) => {
+    const currentDate = new Date().toISOString().slice(0, 10);
+
     const query = `
       SELECT *
       FROM trips t
       INNER JOIN child c ON t.c_id = c.c_id
       INNER JOIN parent p ON c.p_id = p.p_id
-      WHERE p.p_number = ?`;
-    
-    return db.query(query, [phoneNumber])
+      WHERE p.p_number = ? AND DATE(t.date) = ?`;
+
+    return db.query(query, [phoneNumber, currentDate])
       .then(([rows, fields]) => {
         return rows;
       })
--- routers/trip.js
+++ routers/trip.js
@@ -1,20 +1,22 @@
+
 const express = require("express");
 const router = express.Router();
 const tripModel = require("../models/trip");
-// GET route for handling authentication
+
+// GET route for retrieving trips by phone number for the current date
 router.get("/trip/:phoneNumber", async (req, res) => {
   const { phoneNumber } = req.params;
   try {
-    // Query the database to check if the user exists based on the phone number
-    const [rows] = await tripModel.getTripByPhoneNumber(phoneNumber);
-    if (rows.length === 0) {
-      // User not found
-      return res.status(404).json({ message: "Data not found" });
+    // Query the database to get trips for the provided phone number and current date
+    const trips = await tripModel.getTripsByPhoneNumber(phoneNumber);
+
+    if (trips.length === 0) {
+      // No trips found for the given phone number
+      return res.status(404).json({ message: "No trips found for the given phone number" });
     }
 
-    console.log(rows);
-    // For simplicity, we'll just return a success message with the user data
-    res.json(rows);
+    // Return the trip data
+    res.json(trips);
   } catch (err) {
     console.error("Error while querying the database:", err);
     res.status(500).json({ message: "Internal server error" });
